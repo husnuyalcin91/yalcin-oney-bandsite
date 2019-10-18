f04aed7338424ad338ed81fe972622aaa631f3cb
Husnu Yalcin Oney <yalcin.oney91@gmail.com>
2019-10-17 22:58:32 -0400

    new section added to html file, small changes in css, and addition of JS file

---
+++ sprint-2/script.js
@@ -0,0 +1,10 @@
+let comment = [
+    { name: 'Theodore Duncan', comment: 'How can someone be so good!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He`s definitely my favorite ever!'},
+    { name: 'Gary Wong', comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He`s so talented! I wish I can ride like him one day so I can really enjoy myself!'},
+    { name: 'Michael Lyons', comment: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'}
+];
+
+function addComment (arr) {
+for(i=0; i<=comment.length; i++) {
+    }
+}
